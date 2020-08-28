d9783271c36a612462e782ea90fca1009ed89839
Mumitrol <11231202+Mumitrol@users.noreply.github.com>
2020-08-28 21:03:47 +0200

    Added first WebSocket

--- Server/src/main/java/org/example/party/Main.java
+++ Server/src/main/java/org/example/party/Main.java
@@ -1,9 +1,47 @@
 package org.example.party;
 
-public class Main {
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
 
-    public static void main(String[] args) {
+import java.net.InetSocketAddress;
+
+public class Main extends WebSocketServer {
+
+
+    public Main(InetSocketAddress address){
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+        webSocket.send("Welcome to the Server.");
 
     }
+    @Override
+    public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+
+    }
+    @Override
+    public void onMessage(WebSocket webSocket, String s) {
+        webSocket.send(s);
+    }
+    @Override
+    public void onError(WebSocket webSocket, Exception e) {
+
+    }
+
+    public void onStart() {
+
+    }
+
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8887;
+
+        WebSocketServer server = new Main(new InetSocketAddress(host, port));
+        server.run();
+    }
+
 
 }
